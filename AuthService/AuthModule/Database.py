00ed513091e36aca4f476c84f1f7d983334c6196
Anton <Antonnilspersson@gmail.com>
2023-05-17 23:01:33 +0200

    Quick fix

--- AuthService/AuthModule/Database.py
+++ AuthService/AuthModule/Database.py
@@ -2,8 +2,6 @@ from AuthModule import GetModel, executeQuery
 
 def GetObjectFromTable(value, table, column):
     def queryFunc(session, base, value, table, column):
-        # Check if value is provided
-        assert value is not None, "Error: No value provided"
         # Get all rows from the specified table and column
         theTable = session.query(GetModel(table)).filter_by(**{column: value}).first()
         # Check if an error occurred during retrieval
